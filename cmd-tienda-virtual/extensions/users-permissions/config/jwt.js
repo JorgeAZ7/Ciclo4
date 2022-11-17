7ef68bc1b8cf15b120f1003a0bc85eab97a7ecfa
JorgeAZ7 <jorge.anaya.z95@gmail.com>
2022-11-17 14:39:50 -0500

    ReactLearning

---
+++ cmd-tienda-virtual/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4af9ef72-4585-46f2-9219-44fd533553ae'
+};
---
+++ consultarApiLocal/js/index.js
@@ -0,0 +1,57 @@
+console.log("Starting")
+
+
+// Bloque Import
+
+
+
+// Bloque Variables y Constantes
+
+
+
+// Bloque Funciones
+
+const listarGuitarras = (resultado) =>{
+
+    resultado.forEach( (articulo) =>{
+        // Destructuring
+        const {id, nombre, descripcion, precio, imagen } = articulo;
+        const { url } = imagen;
+        const contenedorImagen = document.querySelector(".listado");
+        contenedorImagen.innerHTML += `
+                                            <div class="listado-guitarra">
+                                            <img layout='responsive' width="100" height="220" src="${url}" alt="${nombre}">
+                                                
+                                                <div class="listado-contenido">
+                                                    <h3>${nombre}</h3>
+                                                    <p class="listado-descripcion">${descripcion}</p>
+                                                    <p class="listado-precio">$${precio}</p>
+                                                    <a data-guitarra="${id}" class="listado-enlace" href="guitarra.html?id=${id}">
+                                                        Ver Producto
+                                                    </a>
+                                                </div>
+                                            </div>
+
+
+                                            
+                                        `;
+    } );
+};
+
+// Asyncronas
+
+const consultarApi = async () =>{
+try
+{
+    const respuesta = await fetch("https://whispering-wildwood-03076.herokuapp.com/guitarras/");
+    const resultado = await respuesta.json();
+    listarGuitarras(resultado);
+     
+}
+catch (error)
+{
+    console.log("Error: "+error.message);
+}
+};
+
+consultarApi();
